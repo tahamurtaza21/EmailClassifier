cb3f735f85486cf2a4f2025fe6667d3598febd2c
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2025-09-12 00:53:36 +0100

    Added new spring boot project

---
+++ src/test/java/email/emailclassifier/EmailClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package email.emailclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClassifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
